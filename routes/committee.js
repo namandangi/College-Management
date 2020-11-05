8156e7284331b476cbe115a314eaa28dcda13934
namandangi <danginaman55@gmail.com>
2020-11-05 13:01:20 +0530

    add count search & insert routes for committee & department

--- routes/committee.js
+++ routes/committee.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
-const { getAllCommittee } = require('../controllers/committee');
+const { getAllCommittee, addCommittee, searchCommittee, countCommittee } = require('../controllers/committee');
 
 const router = Router();
 
 router.get('/', getAllCommittee);
+router.post('/add', addCommittee);
+router.get('/search', searchCommittee);
+router.get('/count', countCommittee);
 
 module.exports = router;
