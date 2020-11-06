ecaef5c17bcdec61b072857715b4f47f14039b1f
namandangi <danginaman55@gmail.com>
2020-11-06 11:35:28 +0530

    setup eventlistners & load data from api

--- webapp/src/Components/Library.js
+++ webapp/src/Components/Library.js
@@ -1,11 +1,76 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import libraryLogo from "../images/library-lg.png";
 import switchOrder from "../images/switch-order-logo.png";
 import book from "../images/book.png";
 
-class Library extends Component {
-  render() {
+function Library() {
+  const [tag, setTag] = useState('bname');
+  const [order, setOrder] = useState('1');
+  const [noOfBooks, setBookNo] = useState(0);
+  const [noOfAuthors, setAuthorNo] = useState(0);
+  const [noOfEditions, setEditionNo] = useState(0);
+  const [books, setBooks] = useState([]);
+  const [searchVal, setSearchVal] = useState('');
+
+  async function getCount() {
+    try {
+      const doc = await fetch('/api/library/count');
+      const { noOfBooks, noOfAuthors, noOfEditions } = await doc.json();
+      setBookNo(noOfBooks);
+      setAuthorNo(noOfAuthors);
+      setEditionNo(noOfEditions);
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function getBooks() {
+    try {
+      const doc = await fetch(`/api/library?tag=${tag}&order=${order}`);
+      const books = await doc.json();
+      setBooks(books);
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+
+  const handleChange = (e) => {
+    setSearchVal(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (searchVal !== '') {
+      const doc = await fetch(`/api/library/search?tag=bname&filter=${searchVal}`)
+      const result = await doc.json();
+      setBooks(result);
+    }
+    else {
+      const doc = await fetch(`/api/library?tag=bname`)
+      const result = await doc.json();
+      setBooks(result);
+    }
+  };
+  const handleKeyDown = async (e) => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') handleSubmit(e);
+  };
+
+  const handleOrder = (e) => {
+    e.preventDefault();
+    if(order === '1')
+      setOrder('-1');
+    else 
+      setOrder('1');
+  }
+
+  useEffect(() => {
+    getCount();
+    getBooks();
+  }, [ tag, order, noOfBooks, noOfAuthors, noOfEditions]);
+  console.log(searchVal);
     return (
       <div>
         <Row>
@@ -17,50 +82,54 @@ class Library extends Component {
             </p>
             <p className="page-title">LIBRARY</p>
             <p>
-              No. of books: <b>215</b> <br></br>
-              No. of Authors: <b>215</b> <br></br>
-              No. of Editions: <b>215</b>
+              No. of books: <b>{noOfBooks}</b> <br></br>
+              No. of Authors: <b>{noOfAuthors}</b> <br></br>
+              No. of Editions: <b>{noOfEditions}</b>
             </p>
           </Col>
           {/* <Col md={1}></Col> */}
           <Col className="rightside" md={8}>
             <Row>
-              <Col md={1}>Name</Col>
-              <Col md={1}>Author</Col>
-              <Col className="mr-auto" md={1}>
+              <Col md={1} onClick={() => {setTag('bname')}}>
+                Name
+              </Col>
+              <Col md={1} onClick={() => {setTag('author')}}>
+                Author
+              </Col>
+              <Col className="mr-auto" md={1} onClick={() => {setTag('edition')}}>
                 Edition
               </Col>
 
-              <input className="search" type="text" placeholder="Search" />
-              <buttton>
-                {" "}
+              <input className="search" type="text" placeholder="Search" value={searchVal} onChange={handleChange} onKeyDown={handleKeyDown} />
+              <buttton onClick={handleOrder}>
                 <img src={switchOrder} alt=""></img>{" "}
               </buttton>
             </Row>
             <hr></hr>
-            {/* MAIN */}
+            {/* MAIN */}            
             <Row className="homerow justify-content-md-center">
-              <Col md={5} className="pagegrid">
-                <Row>
-                  <Col md={1}>
-                    <span>
-                      <img style={{ marginTop: "30px" }} src={book} alt="" />
-                    </span>
-                  </Col>
-                  <Col>
-                    <br></br>
-                    <p className="grid-title ">Rich Dad, Poor Dad</p>
-                    <span style={{ float: "right" }}> - Robert Kiyosaki</span>
-                  </Col>
-                </Row>
-                <p style={{ position: "absolute", bottom: "0" }}>Edition 5</p>
-              </Col>
+              { books.length > 0 && books.map((el, id) => (
+                <Col md={5} key={id} className="pagegrid" >
+                  <Row>
+                    <Col md={1}>
+                      <span>
+                        <img style={{ marginTop: "30px" }} src={book} alt="" />
+                      </span>
+                    </Col>
+                    <Col>
+                      <br></br>
+                      <p className="grid-title ">{el.bname}</p>
+                      <span style={{ float: "right" }}> - {el.author}</span>
+                    </Col>
+                  </Row>
+                  <p style={{ position: "absolute", bottom: "0" }}>Edition {el.edition}</p>
+                </Col>
+              ))}
             </Row>
           </Col>
         </Row>
       </div>
     );
-  }
 }
 
 export default Library;
